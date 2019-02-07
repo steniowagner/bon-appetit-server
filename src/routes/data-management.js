f4be0bbf5efae5924759c864fdc396aced235cc7
Stenio Wagner <stenio.wagner1@gmail.com>
2019-02-06 23:04:09 -0300

    Updating the HTTP method used on /populate/clear Route to DELETE method

--- src/routes/data-management.js
+++ src/routes/data-management.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const DataManagementController = require("../controllers/data-management-controller");
 
 router.post("/populate", DataManagementController.populate);
-router.post("/clear", DataManagementController.clear);
+router.delete("/clear", DataManagementController.clear);
 
 module.exports = router;
