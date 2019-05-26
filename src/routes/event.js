aac0ab5f12a1193d111bee6b8bd2f835440a33eb
Stenio Wagner <stenio.wagner1@gmail.com>
2019-05-26 14:55:13 -0300

    Uncommenting routes

--- src/routes/event.js
+++ src/routes/event.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const EventController = require("../controllers/event-controller");
 
-// router.post("/batch", EventController.createInBatch);
-// router.post("/", EventController.create);
+router.post("/batch", EventController.createInBatch);
+router.post("/", EventController.create);
 router.get("/", EventController.readAll);
 router.get("/:id", EventController.readById);
-// router.patch("/:id", EventController.update);
-// router.delete("/:id", EventController.delete);
+router.patch("/:id", EventController.update);
+router.delete("/:id", EventController.delete);
 
 module.exports = router;
--- src/routes/review.js
+++ src/routes/review.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 const ReviewController = require("../controllers/review-controller");
 
-// router.post("/batch", ReviewController.createInBatch);
-// router.post("/", ReviewController.create);
+router.post("/batch", ReviewController.createInBatch);
+router.post("/", ReviewController.create);
 router.get("/", ReviewController.readAll);
 router.get("/:id", ReviewController.readById);
-// router.patch("/:id", ReviewController.update);
-// router.delete("/:id", ReviewController.delete);
+router.patch("/:id", ReviewController.update);
+router.delete("/:id", ReviewController.delete);
 
 module.exports = router;
